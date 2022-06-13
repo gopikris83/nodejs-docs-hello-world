eaa195da6535a52f9c4558d9edeb57b5396e34ee
Gopi krishna <gopikrishna.ravichandran@sas.se>
2022-06-13 15:57:52 +0200

    Added html index file

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const http = require('http');
+    fs = require('fs');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
-
+    fs.readFile('./index.html', function (err, html) {
+        if (err) {
+            throw err; 
+        }       
+        http.createServer(function(request, response) {  
+            response.writeHeader(200, {"Content-Type": "text/html"});  
+            response.write(html);  
+            response.end();  
+        }).listen(8000);
+    });
 const port = process.env.PORT || 1337;
 server.listen(port);
 
